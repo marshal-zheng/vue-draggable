866616bcc00d1d1ca1af49013cd5caa4482be0f0
marshal <1536101747@qq.com>
2024-07-08 22:39:20 +0800

    chore: remove unused externalFunction from utils/test.ts

--- lib/utils/test.ts
+++
@@ -1,6 +0,0 @@
-export function externalFunction(componentInstance: any): void {
-  // 调用组件的内部方法
-  if (componentInstance && componentInstance.internalMethod) {
-    componentInstance.internalMethod();
-  }
-}
